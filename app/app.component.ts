aec868638de1647da2bae76695b4841f26851f6c
dtdenver <dtdenver@hotmail.com>
2017-06-15 14:51:25 -0600

    chap 1 Web Development with Bootstrap 4 and Angular 2

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+// Import the decorator class for Component
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1> Hello, World</h1>'
+})
+export class AppComponent { }
